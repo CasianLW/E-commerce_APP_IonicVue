98ca5741e0d5d2d95001c87c96b78ac4b1fc063d
CasianLW <ciorbacasian@gmail.com>
2023-05-26 17:02:28 +0200

    updated router with all needed routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,95 @@
 import { createRouter, createWebHistory } from "@ionic/vue-router";
-import HomePage from "../views/HomePage.vue";
+import { useAuthStore } from "@/stores/auth.js";
 
 const routes = [
   {
-    path: "/",
-    redirect: "/home",
+    path: "/homepage",
+    name: "Home",
+    component: () => import("@/views/Homepage.vue"),
   },
   {
-    path: "/home",
-    name: "Home",
-    component: HomePage,
+    path: "/subscriptions",
+    name: "Subscriptions",
+    component: () => import("@/views/Subscriptions.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () => import("@/views/Contact.vue"),
+  },
+  {
+    path: "/mentions-legales",
+    name: "MentionsLegales",
+    component: () => import("@/views/MentionsLegales.vue"),
+  },
+  {
+    path: "/cgv",
+    name: "CGV",
+    component: () => import("@/views/CGV.vue"),
+  },
+  {
+    path: "/politique-confidentialite",
+    name: "PolitiqueConfidentialite",
+    component: () => import("@/views/PolitiqueConfidentialite.vue"),
+  },
+  {
+    path: "/account",
+    name: "Account",
+    component: () => import("@/views/Account.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("@/views/Dashboard.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/Login.vue"),
+    meta: {
+      requiresGuest: true,
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("@/views/Register.vue"),
+    meta: {
+      requiresGuest: true,
+    },
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/homepage",
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  if (to.meta.requiresAuth && !authStore.loggedIn) {
+    return next({
+      path: "/login",
+    });
+  }
+
+  if (to.meta.requiresGuest && authStore.loggedIn) {
+    return next({
+      path: "/homepage",
+    });
+  }
+
+  next();
 });
 
 export default router;
