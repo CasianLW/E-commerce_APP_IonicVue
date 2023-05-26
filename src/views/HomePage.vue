d866ccf3dcaaa330d0ea748c6aa21ccf00323481
CasianLW <ciorbacasian@gmail.com>
2023-05-26 17:03:06 +0200

    Homepage added goback btn

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,61 +2,76 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-buttons slot="start">
+          <ion-back-button v-if="canGoBack" default-href="/"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Homepage</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-back-button
+              v-if="canGoBack"
+              default-href="/"
+            ></ion-back-button>
+          </ion-buttons>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <div id="container">
         <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p>
+          Start with Ionic
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+import { onBeforeMount, ref } from "vue";
+import { useIonRouter } from "@ionic/vue";
 
 export default {
   components: {
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar
-  }
-}
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonBackButton,
+    IonButtons,
+  },
+  setup() {
+    const ionRouter = useIonRouter();
+    const canGoBack = ref(false);
+
+    onBeforeMount(() => {
+      canGoBack.value = ionRouter.canGoBack();
+    });
+
+    return {
+      canGoBack,
+    };
+  },
+};
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-
-  color: #8c8c8c;
-
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
