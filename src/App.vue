23d8e47b2ece76e9f062912c638594353dbf5f80
CasianLW <ciorbacasian@gmail.com>
2023-05-26 11:23:17 +0200

    init base project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script>
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+
+export default {
+  components: { IonApp, IonRouterOutlet }
+}
+</script>
