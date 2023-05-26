53c6d2482961a5fc0504f78b04acbf03966d9b90
CasianLW <ciorbcasian@gmail.com>
2023-05-26 17:02:04 +0200

    mentions & cgv & politique vues base

---
+++ src/views/MentionsLegales.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Mentions Légales</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <p>
+        Notre site internet est conforme à la loi en vigueur et nous vous
+        fournissons les informations requises dans nos mentions légales.
+        <!-- Add your Legal Notices text here -->
+      </p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+export default {
+  name: "MentionsLegales",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonBackButton,
+    IonButtons,
+  },
+};
+</script>
---
+++ src/views/PolitiqueConfidentialite.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Politique de Confidentialité</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <p>
+        Notre politique de confidentialité explique comment nous collectons,
+        utilisons et protégeons vos informations personnelles.
+        <!-- Add your Privacy Policy text here -->
+      </p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+export default {
+  name: "PolitiqueConfidentialité",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonBackButton,
+    IonButtons,
+  },
+};
+</script>
