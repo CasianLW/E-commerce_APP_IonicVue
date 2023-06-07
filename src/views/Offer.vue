146d1f2628cce192f8bf137b9e77d18c32f56864
CasianLW <ciorbacasian@gmail.com>
2023-06-07 18:59:35 +0200

    empty page view

---
+++ src/views/Offer.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Offer</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <p>
+        Page a dev, les cas ou la personne est connecté et a une offre ou bien
+        n'a aucune offre
+        <!-- Add your Privacy Policy text here -->
+      </p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+export default {
+  name: "PolitiqueConfidentialité",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonBackButton,
+    IonButtons,
+  },
+};
+</script>
