d0044f7af9c472847bdb4188be4ee588d13c93c0
CasianLW <ciorbcasian@gmail.com>
2023-06-07 17:01:25 +0200

    Failded payment view

---
+++ src/views/checkout/fail/FailView.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Failed</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <p>Payment failed. Please try again.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+export default {};
+</script>
