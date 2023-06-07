952c1193788c4f1faa572e5100c2d03b418d9321
CasianLW <ciorbacasian@gmail.com>
2023-06-08 01:44:53 +0200

    Template empty pages (needed for router)

---
+++ src/views/Account.vue
@@ -0,0 +1,47 @@
+<template>
+  <ion-page>
+    <ion-back-button
+      style="margin-right: auto"
+      default-href="/"
+    ></ion-back-button>
+
+    <!-- <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Account</ion-title>
+      </ion-toolbar>
+    </ion-header> -->
+    <ion-content class="ion-padding">
+      <p>
+        Page a dev Account
+        <!-- Add your Privacy Policy text here -->
+      </p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+export default {
+  name: "Account",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonBackButton,
+    IonButtons,
+  },
+};
+</script>
