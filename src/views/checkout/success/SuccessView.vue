1290cbac577da5135d146d908a31ef330cd30477
CasianLW <ciorbcasian@gmail.com>
2023-06-07 17:02:07 +0200

    succes payment view

---
+++ src/views/checkout/success/SuccessView.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Success</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <p>Payment successful!</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+export default {};
+</script>
