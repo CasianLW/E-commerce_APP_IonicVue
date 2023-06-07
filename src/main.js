d8cb6381424de5a7d7f18910c1e334c37a922448
CasianLW <ciorbacasian@gmail.com>
2023-06-07 18:59:00 +0200

    ajout style.css personnalisé

--- src/main.js
+++ src/main.js
@@ -23,6 +23,8 @@ import "@ionic/vue/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+// custom css
+import "./theme/style.css";
 
 const pinia = createPinia(); // Define pinia variable here
 
