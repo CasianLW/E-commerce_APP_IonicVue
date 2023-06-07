65a3c54b2e63f8c6a105c9536e7884e680399a27
CasianLW <ciorbcasian@gmail.com>
2023-06-07 23:08:01 +0200

    created base for GameList Component

---
+++ src/components/homepage/GameListComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <ion-card>
+    <ion-card-header>
+      <ion-card-title>Game List</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content> </ion-card-content>
+  </ion-card>
+</template>
+
+<script>
+import { IonCard, IonCardHeader, IonCardTitle } from "@ionic/vue";
+
+export default {
+  name: "GameListComponent",
+  data() {
+    return {
+      games: [
+        {
+          title: "Survival Island",
+          thumbnailType: "large",
+          category: ["FPS"],
+          image: "@/ressources/games/gameImage1",
+        },
+        {
+          title: "Wifa 2028",
+          thumbnailType: "medium",
+          category: ["RPG"],
+          image: "@/ressources/games/gameImage1",
+        },
+        {
+          title: "Beach Run",
+          thumbnailType: "small",
+          category: ["FPS", "SANDBOX"],
+          image: "@/ressources/games/gameImage1",
+        },
+        {
+          title: "Lost Mario",
+          thumbnailType: "square",
+          category: ["RPG", "SANDBOX"],
+          image: "@/ressources/games/gameImage1",
+        },
+        {
+          title: "Simulator",
+          thumbnailType: "large",
+          category: ["SANDBOX"],
+          image: "@/ressources/games/gameImage1",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* styles */
+</style>
