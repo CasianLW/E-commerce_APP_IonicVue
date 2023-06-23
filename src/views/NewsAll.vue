08b3da414c601968c61448822d77e5be3e75a46f
CasianLW <ciorbacasian@gmail.com>
2023-06-24 01:17:03 +0200

    base vue for all articles page

---
+++ src/views/NewsAll.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-header>
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-back-button default-href="/home"></ion-back-button>
+          </ion-buttons>
+          <ion-title class="ion-text-center ion-align-items-center"
+            >News</ion-title
+          >
+        </ion-toolbar>
+      </ion-header>
+      <main class="ion-padding"></main>
+    </ion-content>
+  </ion-page>
+</template>
