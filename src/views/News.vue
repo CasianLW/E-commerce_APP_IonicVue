0db44e2f3b9a2d07d8b460135e1668f5e1e6c031
CasianLW <ciorbcasian@gmail.com>
2023-06-23 17:36:18 +0200

    top categories slider & start of news lists filter

--- src/views/News.vue
+++ src/views/News.vue
@@ -18,7 +18,40 @@
         </div>
         <div class="actualites">
           <div class="categories">
-            <div style="background-image: url()"></div>
+            <div class="categorie">
+              <div class="image-container">
+                <img
+                  src="@/ressources/actualites/gamingImage.jpg"
+                  alt="gaming image"
+                />
+              </div>
+              <p class="categorie-title">Gaming</p>
+            </div>
+            <div class="categorie">
+              <div class="image-container">
+                <img
+                  src="@/ressources/actualites/nftImage.jpg"
+                  alt="nft image"
+                />
+              </div>
+              <p class="categorie-title">NFT</p>
+            </div>
+            <div class="categorie">
+              <div class="image-container">
+                <img
+                  src="@/ressources/actualites/cryptoImage.jpg"
+                  alt="crypto image"
+                />
+              </div>
+              <p class="categorie-title">Crypto</p>
+            </div>
+          </div>
+          <div class="news">
+            <div class="news-choice">
+              <div class="active-element"></div>
+              <div class="active-text">Tendances</div>
+              <div>Récents</div>
+            </div>
           </div>
         </div>
         <div></div>
@@ -38,7 +71,7 @@ import {
   IonButtons,
 } from "@ionic/vue";
 export default {
-  name: "PolitiqueConfidentialité",
+  name: "News",
   components: {
     IonContent,
     IonHeader,
@@ -48,10 +81,73 @@ export default {
     IonBackButton,
     IonButtons,
   },
+  data() {
+    return {
+      activeChoice: "Tendances", // This will hold the current active choice
+    };
+  },
+  methods: {
+    setActiveChoice(choice) {
+      this.activeChoice = choice;
+    },
+  },
 };
 </script>
 
 <style scoped>
+.categories {
+  margin-top: 40px;
+  display: flex;
+  overflow-x: auto;
+  scrollbar-width: none; /* Firefox */
+}
+.categories::-webkit-scrollbar {
+  display: none; /* Chrome, Safari, Opera */
+}
+.categorie {
+  position: relative;
+  width: 140px;
+  border-radius: 25px;
+  text-align: center;
+  background-color: transparent;
+  flex: 0 0 auto;
+  margin-right: 16px;
+}
+.categorie-title {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  margin: 0;
+  padding: 8px;
+  background: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.7) 15%,
+    rgba(0, 0, 0, 0) 100%
+  );
+
+  color: white;
+  font-size: 16px;
+  font-weight: 500;
+  border-bottom-left-radius: 25px;
+  border-bottom-right-radius: 25px;
+}
+.image-container {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-top: 100%;
+  overflow: hidden;
+  border-radius: inherit;
+}
+.image-container img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 .header-actus p {
   font-weight: 400;
   font-size: 14px;
@@ -81,5 +177,36 @@ ion-content {
       )
       /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */,
     linear-gradient(0deg, #000000, #000000), #7458ea;
+}
+
+/*select news  */
+.news-choice div {
+  font-weight: 500;
+  width: 50%;
+  z-index: 10;
+}
+.news-choice {
+  color: white;
+  background-color: #000000;
+  border-radius: 25px;
+  height: 44px;
+  text-align: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 40px;
+}
+.active-element {
+  transition: all 0.3s ease-in-out;
+  position: absolute;
+  transform: translateX(4px);
+  z-index: 0;
+  width: 45% !important;
+  height: 36px;
+  background-color: #d3fe57;
+  border-radius: 25px;
+}
+.active-text {
+  color: #000000;
 }
 </style>
