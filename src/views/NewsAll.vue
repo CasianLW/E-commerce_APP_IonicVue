b21e6a696e4dff1a36d867e937fba66149886e62
CasianLW <ciorbcasian@gmail.com>
2023-06-24 02:23:11 +0200

    items per page 2 -> 5

--- src/views/NewsAll.vue
+++ src/views/NewsAll.vue
@@ -98,7 +98,7 @@ export default {
     const authStore = useAuthStore();
     const events = ref([]);
     const currentPage = ref(1);
-    const itemsPerPage = 2;
+    const itemsPerPage = 5;
     const loaded = ref(false);
 
     onMounted(async () => {
